2d77fcfb9b79a2c3f73b8a4acfdd1c15303128e0
Ваше Имя <klypcontacts@gmail.com>
2026-09-11 22:13:28 +0200

    Implement flip_coin function with Gaussian distribution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from random import choice
+from typing import Dict
+
+
+def flip_coin() -> Dict[int, float]:
+    trials = 10000
+    counts = {i: 0 for i in range(11)}
+
+    for _ in range(trials):
+        heads = sum(choice([0, 1]) for _ in range(10))
+        counts[heads] += 1
+
+    return {k: round((v / trials) * 100, 2) for k, v in counts.items()}
